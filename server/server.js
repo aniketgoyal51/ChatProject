da0219fc79ad80b7b454ffba5c5dd15f4940b7aa
aniketgoyal51 <141112246+aniketgoyal51@users.noreply.github.com>
2024-08-28 16:29:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { app ,server} = require('./Socket/Socket');
 app.use(express.json())
 require("dotenv").config()
 
-app.use(cors())
+app.use(cors({origin:"https://chat-project-n1meirq12-aniketgoyal51s-projects.vercel.app"}))
 
 mongoose.connect(process.env.VITE_DATABASE_URL,{
     dbName: 'Chat'
